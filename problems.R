3d75678a3f2eb740a41435ef181bbbe86639ca51
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-01-21 12:26:42 +0000

    New issue relating to $

--- problems.R
+++ problems.R
@@ -3,6 +3,13 @@
 
 # EXTRACTING A VECTOR FROM A DATAFRAME OF CHARACTERS
 # --- Problem ---
+row.element <- 1:5
+numbers.df <- data.frame (c1=rep (1, 5),
+                          c2=rep (2, 5),
+                          c3=rep (3, 5),
+                          c4=rep (4, 5),
+                          c5=rep (5, 5))
+all (row.element %in% numbers.df[1,])
 # simple data frame of letters and an example of one of the rows
 row.element <- c ('a', 'b', 'c', 'd', 'e')
 letters.df <- data.frame (c1=rep ('a', 5),
@@ -14,6 +21,7 @@ letters.df <- data.frame (c1=rep ('a', 5),
 all (row.element %in% letters.df[1,])  # FALSE!
 # let's try converting to a character vector ....
 as.character (as.vector (letters.df[1, ]))  # No, list of 1s
+all (row.element %in% as.character (as.vector (letters.df[1, ]))) 
 # --- Solution 1 ---
 # OK.... let's try dropping the data.frame class,
 #  unlist the resulting list object and then convert to character
@@ -30,3 +38,25 @@ letters.df <- data.frame (c1=rep ('a', 5),
                           c5=rep ('e', 5),
                           stringsAsFactors=FALSE)
 all (row.element %in% letters.df[1,])  # TRUE!
+
+# USING THE $ SIGN
+# Here is a list
+mylst <- list("apple"=1, "pear"=3, "orange"=5, "orangerie"=10)
+# We can extract elements of that list using the $
+mylst$apple
+# But did you know you could extract those elements also with the beginning of
+#  the slot name, so long as its unique?
+mylst$a  # works
+mylst$o  # doesn't work because two slots begin with an o
+# This is great, it saves typing.... but, it can cause unexpected, albeit rare, errors
+# One such example is if I were looping through multiple lists and I first wanted to check
+# if the the list had a value for the slot name, I might do this like...
+!is.null(mylst$apple)
+# This works except in the sitatuation where my lists my contain orange and not orangerie
+!is.null(mylst$orange)
+# The above example would always return TRUE, even if there was no orange in the list
+# A way to avoid this is to use [['']]
+!is.null(mylst[-3][['orange']])
+# Now we get false, which is what we'd expect.
+# So the lesson here is the $ sign is useful but can be dangerous. I'd always suggest to use
+# [['']] indexing within functions to avoid this potential issue with autocomplete
